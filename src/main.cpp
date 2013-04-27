9246c837f39ed2697d2a755bd47b336614c84241
fo2rist <fo2@inbox.ru>
2013-04-27 16:00:07 +0400

    Version 1.1.0 (4) Migration to new SDK version.

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,13 @@
 #include <QLocale>
 #include <QTranslator>
 
+// include JS Debugger / CS Profiler enabler
+// this feature is enabled by default in the debug build only
+#include <Qt/qdeclarativedebug.h>
+
 using namespace bb::cascades;
 
-int main(int argc, char **argv)
+Q_DECL_EXPORT int main(int argc, char **argv)
 {
     // this is where the server is started etc
     Application app(argc, argv);
